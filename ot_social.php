a3900622f9f570fc0d46aa83d31ad0dbf68f110e
jobayer007 <jobayer007@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 09:45:56 +0000

    uploading version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/ot-social-icons/trunk@1216994 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ot_social.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: OT Social Icons
+Plugin URI: http://orangeit-info.com
+Description: You can show your social icons in your widget area.It is an awesome design.
+Tags: social icons, social icon, social widget icon, social, icons, icon, social widgets, facebook icon, twitter icon, google-plus icons
+Author: Jobayer
+Author URI: http://freelancer-jobayer.com
+version: 1.0
+License: GPLv2 or later
+Text Domain: ot_social_icons
+*/
+add_action('wp_enqueue_scripts', function(){
+	wp_register_style('OTstyle', plugins_url('/css/style.css', __FILE__));
+	wp_register_style('OTreset', plugins_url('/css/reset.css', __FILE__));
+	wp_register_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
+	
+	wp_enqueue_style('OTstyle');
+	wp_enqueue_style('OTreset');
+	wp_enqueue_style('font-awesome');
+});
+class ot_social extends WP_Widget{
+	function __construct(){
+		$params = array(
+			'description' => 'You can show your social icons  in your widget area.',
+			'name' => 'OT Social Icons'
+		);
+		parent::__construct('ot_social', '', $params);
+	}
+	public function form($instance){
+		extract($instance);
+		$instance = wp_parse_args( (array) $instance, array( 'facebook' => 'DEFAULT_VALUE_HERE' ) );
+		?>
+		<p>Add you All Social Link icons bellow the social text box</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('facebook');?>">Facebook</label>
+			<input type="text" placeholder="http://facebook.com" class="widefat" id="<?php echo $this->get_field_id('facebook');?>"
+					name="<?php echo $this->get_field_name('facebook'); ?>"
+					value="<?php if(isset($facebook)) echo esc_attr($facebook); ?>"
+			/>
+			<p>Add you Facebook link</p>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('twitter');?>">Twitter</label>
+			<input type="text" placeholder="http://twitter.com" class="widefat" id="<?php echo $this->get_field_id('twitter');?>"
+					name="<?php echo $this->get_field_name('twitter'); ?>"
+					value="<?php if(isset($twitter)) echo esc_attr($twitter); ?>"
+			/>
+			<p>Add you Twitter link</p>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('googleplus');?>">Google Plus</label>
+			<input type="text" placeholder="http://plug.google.com" class="widefat" id="<?php echo $this->get_field_id('googleplus');?>"
+					name="<?php echo $this->get_field_name('googleplus'); ?>"
+					value="<?php if(isset($googleplus)) echo esc_attr($googleplus); ?>"
+			/>
+			<p>Add you Google Plus link</p>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('pinterest');?>">Pinterest</label>
+			<input type="text" placeholder="http://pinterest.com" class="widefat" id="<?php echo $this->get_field_id('pinterest');?>"
+					name="<?php echo $this->get_field_name('pinterest'); ?>"
+					value="<?php if(isset($pinterest)) echo esc_attr($pinterest); ?>"
+			/>
+			<p>Add you Pinterest link</p>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('linkedin');?>">Linked In</label>
+			<input type="text" placeholder="http://linkedin.com" class="widefat" id="<?php echo $this->get_field_id('linkedin');?>"
+					name="<?php echo $this->get_field_name('linkedin'); ?>"
+					value="<?php if(isset($linkedin)) echo esc_attr($linkedin); ?>"
+			/>
+			<p>Add you Linked In link</p>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('instagram');?>">Instagram</label>
+			<input type="text" placeholder="http://instragram.com" class="widefat" id="<?php echo $this->get_field_id('instagram');?>"
+					name="<?php echo $this->get_field_name('instagram'); ?>"
+					value="<?php if(isset($instagram)) echo esc_attr($instagram); ?>"
+			/>
+			<p>Add you Instagram link</p>
+		</p>
+		<?php
+		
+	}
+	
+	public function widget($args, $instance){
+		extract($args);
+		extract($instance);
+		$otsocial = '
+					<div class="container">
+						 <div class="row">
+							<a href="'.$facebook.'" target="_blank"><i class="fa fa-facebook"></i></a>
+							<a href="'.$twitter.'" target="_blank"><i class="fa fa-twitter"></i></a>
+							<a href="'.$googleplus.'" target="_blank"><i class="fa fa-google-plus"></i></a>
+							<a href="'.$pinterest.'" target="_blank"><i class="fa fa-pinterest"></i></a>
+							<a href="'.$linkedin.'" target="_blank"><i class="fa fa-linkedin"></i></a>
+							<a href="'.$instagram.'" target="_blank"><i class="fa fa-instagram"></i></a>
+						</div>
+					</div>';
+		echo $args[before_widget].$otsocial.$args['after_widget'];
+	}
+	
+}
+add_action('widgets_init', 'ot_social_widget');
+function ot_social_widget(){
+	register_widget('ot_social');
+	
+}
---
+++ readme.txt
@@ -0,0 +1,85 @@
+=== Plugin Name ===
+Donate link: http://orangeit-info.com
+Tags: social icons, social icon, social widget icon, social, icons, icon, social widgets, facebook icon, twitter icon, google-plus icons
+Requires at least: 3.4
+Tested up to: 4.2.4
+Stable tag: 4.2.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+
+== Description ==
+
+You can show your social icons in your widget area.It is an awesome design.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+
+1. Upload `ot_social_icons` folder to the `/wp-content/plugins/` directory or you can upload from your WordPress Admin from "Add new Plugin" menu from Plugin
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to widget from Appearance then just drag and drop "OT Social Icon" on your any widget area. 
+4. Add your all Social Link Icons like as facebook, twitter, google-plus etc
+
+== Frequently Asked Questions ==
+
+= Can i add my pinterest link =
+
+Yes! You can add your pinterest link
+
+= Can i add my instagram link =
+
+Yes, You can add your instagram link
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 1.0 =
+This is the first version of this plugin
+
+= 0.5 =
+* List versions from most recent at top to oldest at bottom.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Upgrade notices describe the reason a user should upgrade.  No more than 300 characters.
+
+= 0.5 =
+This version fixes a security related bug.  Upgrade immediately.
+
+== Arbitrary section ==
+
+You may provide arbitrary sections, in the same format as the ones above.  This may be of use for extremely complicated
+plugins where more information needs to be conveyed that doesn't fit into the categories of "description" or
+"installation."  Arbitrary sections will be shown below the built-in sections outlined above.
+
+== A brief Markdown Example ==
+
+Ordered list:
+
+1. Some feature
+1. Another feature
+1. Something else about the plugin
+
+Unordered list:
+
+* something
+* something else
+* third thing
+
+Here's a link to [WordPress](http://wordpress.org/ "Your favorite software") and one to [Markdown's Syntax Documentation][markdown syntax].
+Titles are optional, naturally.
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
+
+Markdown uses email style notation for blockquotes and I've been told:
+> Asterisks for *emphasis*. Double it up  for **strong**.
+
+`<?php code(); // goes in backticks ?>`
